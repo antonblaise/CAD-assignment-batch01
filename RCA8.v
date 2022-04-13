9b71b893e5c50bccee5544990fb18182341f878f
antonblaise <antoniusblaise@gmail.com>
2022-04-13 13:12:40 +0800

    Update RCA8.v

--- RCA8.v
+++ RCA8.v
@@ -12,7 +12,7 @@ module RCA8 (
 
 	wire c;
 	
-	RCA4 u0 (.A(a[3:0]), .B(b[3:0] ^ (sub ? 8'hF : 8'h0)), .Cin(sub), .S(z[3:0]), .Cout(c));
-	RCA4 u1 (.A(a[7:4]), .B(b[7:4] ^ (sub ? 8'hF : 8'h0)), .Cin(c), .S(z[7:4]), .Cout(Cout));
+	RCA4 u0 (.A(a[3:0]), .B(b[3:0] ^ (sub ? 4'hF : 4'h0)), .Cin(sub), .S(z[3:0]), .Cout(c));
+	RCA4 u1 (.A(a[7:4]), .B(b[7:4] ^ (sub ? 4'hF : 4'h0)), .Cin(c), .S(z[7:4]), .Cout(Cout));
 
 endmodule
